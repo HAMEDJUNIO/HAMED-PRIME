d5a050fcfe8b807d7806527d38320ad0e389871c
HAMEDJUNIO <shidoruprime@gmail.com>
2024-09-29 20:24:36 +0000

    Update pinterest.js

--- scripts/cmds/pinterest.js
+++ scripts/cmds/pinterest.js
@@ -1,71 +1,141 @@
 const axios = require("axios");
+
 const fs = require("fs-extra");
+
 const path = require("path");
 
+
+
 module.exports = {
-	config: {
-		name: "pinterest",
-		aliases: ["pin", "Pint"],
-		version: "1",
-		author: "Aesther",
-		countDown: 5,
-		role: 0,
-		shortDescription: {
-			vi: "Image Pinterest Search 📷",
-			en: "Image Pinterest Search 📸"
-		},
-		longDescription: {
-			en: "Pinterest image search."
-		},
-		category: "image",
-		guide: {
-			en: "{prefix}pinterest <search query> -<number of images>"
-		}
-	},
 
-	onStart: async function ({ api, event, args, usersData }) {
-		try {
-			const userID = event.senderID;
+  config: {
 
-			const keySearch = args.join(" ");
-			if (!keySearch.includes("-")) {
-				return api.sendMessage(`🔴 𝘽𝙍𝙐𝙃𝙃 🔴\n-------------------------------\nExample: Pinterest Kakashi - 10\n (Specify how many images you want to appear in the result)`, event.threadID, event.messageID);
-			}
-			const keySearchs = keySearch.substr(0, keySearch.indexOf('-')).trim();
-			const numberSearch = parseInt(keySearch.split("-").pop().trim()) || 6;
+    name: "pinterest",
 
-			const res = await axios.get(`https://celestial-dainsleif-v2.onrender.com/pinterest?pinte=${encodeURIComponent(keySearchs)}`);
-			const data = res.data;
+    aliases: ["pin"],
 
-			if (!data || !Array.isArray(data) || data.length === 0) {
-				return api.sendMessage(`No image data found for "${keySearchs}". Please try another search query.`, event.threadID, event.messageID);
-			}
+    version: "1.0.2",
 
-			const imgData = [];
+    author: "JVB",
 
-			for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
-				const imageUrl = data[i].image;
+    role: 0,
 
-				try {
-					const imgResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-					const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
-					await fs.outputFile(imgPath, imgResponse.data);
-					imgData.push(fs.createReadStream(imgPath));
-				} catch (error) {
-					console.error(error);
-					// Handle image fetching errors (skip the problematic image)
-				}
-			}
+    countDown: 50,
 
-			await api.sendMessage({
-				attachment: imgData,
-				body: `-- 𝙋𝙄𝙉𝙏𝙀𝙍𝙀𝙎𝙏 -- ☂️ [${imgData.length}]\n[📱] "${keySearchs}":`
-			}, event.threadID, event.messageID);
+    shortDescription: {
 
-			await fs.remove(path.join(__dirname, 'cache'));
-		} catch (error) {
-			console.error(error);
-			return api.sendMessage(`An error occurred. Please try again later.`, event.threadID, event.messageID);
-		}
-	}
+      en: "Search for images on Pinterest"
+
+    },
+
+    longDescription: {
+
+      en: ""
+
+    },
+
+    category: "image",
+
+    guide: {
+
+      en: " {prefix} Pinterest <nom de l'image recherché> - <nombre d' image>"
+
+    }
+
+  },
+
+
+
+  onStart: async function ({ api, event, args, usersData }) {
+
+    try {
+
+      const userID = event.senderID;
+
+
+
+      const keySearch = args.join(" ");
+
+      if (!keySearch.includes("-")) {
+
+        return api.sendMessage(`𝐬'𝐢𝐥 𝐭𝐞 𝐩𝐥𝐚𝐢̂𝐭 𝐞𝐧𝐭𝐫𝐞 𝐥𝐞 𝐧𝐨𝐦𝐛𝐫𝐞 𝐝𝐞 𝐥'𝐢𝐦𝐚𝐠𝐞 𝐫𝐞𝐜𝐡𝐞𝐫𝐜𝐡𝐞́ 
+
+:${this.config.guide.en}`, event.threadID, event.messageID);
+
+      }
+
+      const keySearchs = keySearch.substr(0, keySearch.indexOf('-')).trim();
+
+      const numberSearch = parseInt(keySearch.split("-").pop().trim()) || 6;
+
+
+
+      const res = await axios.get(`https://celestial-dainsleif-v2.onrender.com/pinterest?pinte=${encodeURIComponent(keySearchs)}`);
+
+      const data = res.data;
+
+
+
+      if (!data || !Array.isArray(data) || data.length === 0) {
+
+        return api.sendMessage(`𝐀𝐮𝐜𝐮𝐧𝐞 𝐢𝐦𝐚𝐠𝐞 𝐚 𝐞́𝐭𝐞́ 𝐭𝐫𝐨𝐮𝐯𝐞𝐫 "${keySearchs}"veuillez recherchez autre chose 🕵️📌`, event.threadID, event.messageID);
+
+      }
+
+
+
+      const imgData = [];
+
+
+
+      for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
+
+        const imageUrl = data[i].image;
+
+
+
+        try {
+
+          const imgResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+          const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
+
+          await fs.outputFile(imgPath, imgResponse.data);
+
+          imgData.push(fs.createReadStream(imgPath));
+
+        } catch (error) {
+
+          console.error(error);
+
+          // Handle image fetching errors (skip the problematic image)
+
+        }
+
+      }
+
+
+
+      await api.sendMessage({
+
+        attachment: imgData,
+
+        body: `𝐯𝐨𝐢𝐜𝐢 𝐪𝐮𝐞𝐥𝐪𝐮𝐞𝐬 𝐢𝐦𝐚𝐠𝐞𝐬 𝐩𝐨𝐮𝐫 𝐯𝐨𝐮𝐬 ${imgData.length}𝐥𝐞𝐬 𝐫𝐞𝐬𝐮𝐥𝐭𝐚𝐭𝐬 𝐝𝐞 𝐥'𝐢𝐦𝐠𝐞 𝐝𝐮 𝐧𝐨𝐦 𝐝𝐞 ➪"${keySearchs}📸📌":`
+
+      }, event.threadID, event.messageID);
+
+
+
+      await fs.remove(path.join(__dirname, 'cache'));
+
+    } catch (error) {
+
+      console.error(error);
+
+      return api.sendMessage(`An error occurred. Please try again later.`, event.threadID, event.messageID);
+
+    }
+
+  }
+
 };
